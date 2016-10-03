262c41ae4c8073748292f0dcc05ef799ba44f53c
ryuunoakaihitomi <18475860917@163.com>
2016-10-03 00:33:46 -0500

    Update MainActivity.java

--- app/src/main/java/com/ryuunoakaihitomi/rebootmenu/MainActivity.java
+++ app/src/main/java/com/ryuunoakaihitomi/rebootmenu/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.*;
 import java.io.*;
 import java.util.*;
 import java.lang.Process;
+import android.content.pm.PackageManager.*;
 public class MainActivity extends Activity 
 {
 	boolean state;
@@ -15,10 +16,10 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		final AlertDialog.Builder ab = new AlertDialog.Builder(this, AlertDialog.THEME_DEVICE_DEFAULT_DARK);
+		final AlertDialog.Builder ab=new AlertDialog.Builder(this, AlertDialog.THEME_DEVICE_DEFAULT_DARK);	
 		ab.setTitle("高级电源菜单↓");
 		final String[]rebootText={"重启", "svc power reboot","关机","svc power shutdown","Recovery恢复模式","svc power reboot recovery","Bootloader启动引导模式(Fastboot)","svc power reboot bootloader","热重启","setprop ctl.restart zygote","重启用户界面","busybox pkill com.android.systemui","安全模式","setprop persist.sys.safemode 1","锁屏","input keyevent 26"};
-		final String[]rebootText2={"重启*", "reboot","关机*","reboot -p","Recovery恢复模式*","reboot recovery","Bootloader启动引导模式(Fastboot)*","reboot bootloader","热重启","setprop ctl.restart zygote","重启用户界面","busybox pkill com.android.systemui","安全模式*","setprop persist.sys.safemode 1","锁屏","input keyevent 26"};
+		final String[]rebootText2={"重启*", "reboot","关机*","reboot -p","Recovery恢复模式*","reboot recovery","Bootloader启动引导模式(Fastboot)*","reboot bootloader","热重启","setprop ctl.restart zygote","重启用户界面","busybox pkill com.android.systemui","安全模式","setprop persist.sys.safemode 1","锁屏","input keyevent 26"};
 		final String[]s=new String[8];
 		final String[]s2=new String[8];
 		for (int i=0;i < 8;i++)
@@ -62,14 +63,7 @@ public class MainActivity extends Activity
 							cmdExec(c);
 							if (c == rebootText[13])
 							{
-								if (!state)
-								{
-									c = rebootText[1];
-								}
-								else
-								{
-									c = rebootText2[1];
-								}
+								c = rebootText[1];
 								cmdExec(c);
 							}
 							Toast.makeText(getApplicationContext(), "命令已发送。。。如果这片烤面包片消失之后手机还是没有反应，应该是执行失败了。请到帮助那里查看解决方法", Toast.LENGTH_LONG).show();
@@ -104,7 +98,7 @@ public class MainActivity extends Activity
 					Toast.makeText(getApplicationContext(), "2.默认模式中的重启&关机：调用Android系统电源管理模块.速度慢,有一点执行失败的可能性(一直显示正在xx的对话),但是更加安全.", Toast.LENGTH_LONG).show();
 					Toast.makeText(getApplicationContext(), "3.recovery:恢复模式.默认模式中会先调用默认重启.\n4：bootloader,亦称fastboot模式,线刷模式.默认模式中会先调用默认重启.\n5.热重启,在保持设备开机的情况下,关闭并重新加载系统.", Toast.LENGTH_LONG).show();
 					Toast.makeText(getApplicationContext(), "6.重启用户界面,重启com.android.systemui,用于刷新桌面UI,重新加载桌面小部件或刷新图标显示.\n7.安全模式：重启系统停用所有第三方应用,用于排查有问题的应用并恢复系统.\n8.锁屏：熄灭屏幕", Toast.LENGTH_LONG).show();
-					Toast.makeText(getApplicationContext(), "(≧∇≦)/\n\n版本" + getAppVersionName(getApplicationContext()) + " 20161002", Toast.LENGTH_SHORT).show();
+					Toast.makeText(getApplicationContext(), "(≧∇≦)/\n\n版本" + getAppVersionName(getApplicationContext()) + " 20161003", Toast.LENGTH_SHORT).show();
 					alphaShow(ab.create(), 0.75f);
 				}
 			});
