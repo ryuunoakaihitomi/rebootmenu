e6d4ecd98974d281e1bb0c427ec49af65041e4c4
ryuunoakaihitomi <18475860917@163.com>
2018-02-17 09:18:28 +0800

    禁用xposed 删除不必要的存储权限

---
+++ app/src/main/java/com/ryuunoakaihitomi/rebootmenu/MyApplication.java
@@ -0,0 +1,31 @@
+package com.ryuunoakaihitomi.rebootmenu;
+
+import android.app.Application;
+
+import com.ryuunoakaihitomi.rebootmenu.util.DebugLog;
+
+import java.lang.reflect.Field;
+
+/**
+ * 自分のアプリケーションクラス
+ * Created by ZQY on 2018/2/17.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        new DebugLog("起きます");
+        //エクスポスドを無効にしてみて
+        try {
+            Field field = ClassLoader.getSystemClassLoader()
+                    .loadClass("de.robv.android.xposed.XposedBridge")
+                    .getDeclaredField("disableHooks");
+            field.setAccessible(true);
+            field.set(null, true);
+        } catch (Throwable e) {
+            new DebugLog("エラーメッセージある：マイアプリから", DebugLog.W);
+            e.printStackTrace();
+        }
+    }
+}
