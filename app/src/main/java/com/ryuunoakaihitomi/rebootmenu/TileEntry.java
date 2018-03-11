f255a958d86347cf508a26881eaa96e3481fa147
ryuunoakaihitomi <18475860917@163.com>
2018-03-11 22:54:33 +0800

    性能优化 为Tile方式打开应用带来缩短几毫秒时间的速度提升

--- app/src/main/java/com/ryuunoakaihitomi/rebootmenu/TileEntry.java
+++ app/src/main/java/com/ryuunoakaihitomi/rebootmenu/TileEntry.java
@@ -1,6 +1,7 @@
 package com.ryuunoakaihitomi.rebootmenu;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
@@ -14,7 +15,7 @@ import android.service.quicksettings.TileService;
 public class TileEntry extends TileService {
     @Override
     public void onClick() {
-        startActivityAndCollapse(getPackageManager().getLaunchIntentForPackage(getPackageName()));
+        startActivityAndCollapse(new Intent(this, MainActivity.class));
     }
 
     //这是一个随时可用的磁贴，因此应该在可见时时刻保持“活跃状态”（对比突出图标）
