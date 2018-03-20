208292b0f586f115937f7bf352132e0feab0a3c5
ryuunoakaihitomi <18475860917@163.com>
2018-03-21 00:34:16 +0800

    因为作者需要 增加音量调节界面弹出功能

---
+++ app/src/main/java/com/ryuunoakaihitomi/rebootmenu/TileLongPressEntry.java
@@ -0,0 +1,28 @@
+package com.ryuunoakaihitomi.rebootmenu;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.media.AudioManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.ryuunoakaihitomi.rebootmenu.util.TextToast;
+
+/**
+ * To set the volume.(Hidden feature)
+ * Created by ZQY on 2018/3/20.
+ */
+
+public class TileLongPressEntry extends Activity {
+
+    @SuppressWarnings("ConstantConditions")
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new TextToast(this, true, "クイック設定タイルを長押し:音量調節");
+        AudioManager audioManager = getSystemService(AudioManager.class);
+        audioManager.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
+        finish();
+    }
+}
