91ccc3cba7f8446133da3e89991abc2e54636044
ryuunoakaihitomi <18475860917@163.com>
2018-04-29 12:34:30 +0000

    修正之前的命令行字符串转移留下的bug

--- app/src/main/java/com/ryuunoakaihitomi/rebootmenu/util/Commands.java
+++ app/src/main/java/com/ryuunoakaihitomi/rebootmenu/util/Commands.java
@@ -6,21 +6,22 @@ package com.ryuunoakaihitomi.rebootmenu.util;
  * Created by ZQY on 2018/4/27.
  */
 
-
 public class Commands {
+    private static final String _RECOVERY = " recovery",
+            _BOOTLOADER = " bootloader",
+            SVC_POWER_ = "svc power ",
+            SETPROP_ = "setprop ";
     //明明需要public的，AS偏偏就给我个Access can be private.
     @SuppressWarnings("WeakerAccess")
     public static final String
-            SVC_POWER_ = "svc power ",
             REBOOT = SVC_POWER_ + "reboot",
             REBOOT_F = "reboot",
             SHUTDOWN = SVC_POWER_ + "shutdown",
             SHUTDOWN_F = REBOOT_F + " -p",
-            RECOVERY = SVC_POWER_ + "recovery",
-            RECOVERY_F = REBOOT_F + " recovery",
-            BOOTLOADER = SVC_POWER_ + "bootloader",
-            BOOTLOADER_F = REBOOT_F + " bootloader",
-            SETPROP_ = "setprop ",
+            RECOVERY = SVC_POWER_ + REBOOT_F + _RECOVERY,
+            RECOVERY_F = REBOOT_F + _RECOVERY,
+            BOOTLOADER = SVC_POWER_ + REBOOT_F + _BOOTLOADER,
+            BOOTLOADER_F = REBOOT_F + _BOOTLOADER,
             HOT_REBOOT = SETPROP_ + "ctl.restart zygote",
             RESTART_SYSTEM_UI = "busybox pkill com.android.systemui",
             RESTART_SYSTEM_UI_ALTERNATIVE = "killall com.android.systemui",
