98f2119992fba8c61cc1ab3c79e1541c6559b675
ryuunoakaihitomi <22442525+ryuunoakaihitomi@users.noreply.github.com>
2021-09-07 23:38:01 +0800

    R3.12.1 在Android 12上禁用尚未与此兼容的华为分析服务 更新Toasty，加入了随深色模式自动变化的normal主题

--- app/src/normal/java/github/ryuunoakaihitomi/powerpanel/stat/InternalDoerImpl.kt
+++ app/src/normal/java/github/ryuunoakaihitomi/powerpanel/stat/InternalDoerImpl.kt
@@ -1,33 +1,41 @@
 package github.ryuunoakaihitomi.powerpanel.stat
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
 import com.huawei.hms.analytics.HiAnalytics
 import com.huawei.hms.analytics.HiAnalyticsTools
+import github.ryuunoakaihitomi.powerpanel.BuildConfig
 import timber.log.Timber
 
 /**
- * 目前使用的是`Firebase`
+ * 目前使用的是`Firebase`和`Huawei AppGallery Collect`
  */
 object InternalDoerImpl : InternalDoer {
 
     override fun initialize(ctx: Context) {
-        HiAnalyticsTools.enableLog()
-        HiAnalytics.getInstance(ctx)
+        if (BuildConfig.DEBUG) {
+            HiAnalyticsTools.enableLog()
+        }
+        // FIXME 等待华为分析服务适配Android 12
+        // https://developer.huawei.com/consumer/cn/doc/development/HMSCore-Guides/version-change-history-0000001050705116
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            HiAnalytics.getInstance(ctx)
+        }
     }
 
     override fun setCustomKey(k: String, v: Any) {
         Firebase.crashlytics.apply {
             when (v) {
                 is String -> setCustomKey(k, v)
-                is Boolean -> setCustomKey(k, v)
                 is Int -> setCustomKey(k, v)
                 is Long -> setCustomKey(k, v)
                 is Float -> setCustomKey(k, v)
                 is Double -> setCustomKey(k, v)
+                is Boolean -> setCustomKey(k, v)
                 is Array<*> -> setCustomKey(k, v.contentToString())
                 else -> Timber.w("Undefined type: $k, $v")
             }
