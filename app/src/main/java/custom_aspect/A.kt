08dc986e37615c85eda33d8d9bd500e53f044590
ryuunoakaihitomi <22442525+ryuunoakaihitomi@users.noreply.github.com>
2021-11-04 18:20:27 +0800

    fix BUILD_NOTE

--- app/src/main/java/custom_aspect/A.kt
+++ app/src/main/java/custom_aspect/A.kt
@@ -16,12 +16,13 @@ class A {
      * 原因：在WearOS上打开链接会抛出不为[ActivityNotFoundException]的异常
      *
      * `Permission Denial: starting Intent { act=... dat=... cmp=com.google.android.wearable.app/com.google.android.clockwork.wcs.remoteintent.UriRedirectActivity } from ProcessRecord ... requires com.google.android.wearable.READ_SETTINGS`
+     * 后记：似乎在Android 11 Wear OS中得以修复，不再处理链接
      * @see ClickableViewHolder
      */
     @Around("call(* setOnClickListener(..)) && this(com.drakeet.about.ClickableViewHolder)")
     fun fixAboutPageCrashOnWatch(pjp: ProceedingJoinPoint) {
-        val rewListener = pjp.args[0] as View.OnClickListener
-        pjp.proceed(arrayOf(OnClickListenerSafeProxy(rewListener)))
+        val rawListener = pjp.args[0] as View.OnClickListener
+        pjp.proceed(arrayOf(OnClickListenerSafeProxy(rawListener)))
     }
 
     private class OnClickListenerSafeProxy(private val l: View.OnClickListener) :
