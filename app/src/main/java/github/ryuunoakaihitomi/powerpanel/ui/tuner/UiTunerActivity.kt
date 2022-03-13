01801cda11c2502b90ffd5d9d82ca59ed7c6a87e
ryuunoakaihitomi <22442525+ryuunoakaihitomi@users.noreply.github.com>
2022-03-13 19:06:54 +0800

    R3.15.5

--- app/src/main/java/github/ryuunoakaihitomi/powerpanel/ui/tuner/UiTunerActivity.kt
+++ app/src/main/java/github/ryuunoakaihitomi/powerpanel/ui/tuner/UiTunerActivity.kt
@@ -3,8 +3,9 @@ package github.ryuunoakaihitomi.powerpanel.ui.tuner
 import android.os.Bundle
 import androidx.annotation.Keep
 import androidx.appcompat.app.AppCompatActivity
+import com.github.kyuubiran.ezxhelper.utils.showToast
+import github.ryuunoakaihitomi.powerpanel.databinding.ActivityTunerPlaceholderBinding
 import github.ryuunoakaihitomi.powerpanel.stat.Statistics
-import github.ryuunoakaihitomi.powerpanel.util.uiLog
 import kotlin.system.exitProcess
 
 class UiTunerActivity : AppCompatActivity() {
@@ -22,13 +23,13 @@ class UiTunerActivity : AppCompatActivity() {
                 .commit()
             Statistics.logXposedEnabled()
         } else {
-            uiLog("Xposed required")
-            finish()
+            setContentView(ActivityTunerPlaceholderBinding.inflate(layoutInflater).root)
+            showToast("Xposed required")
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (hookedByXposed) exitProcess(0)  // 自杀让lsposed实现生效
+        exitProcess(0)  // 自杀让lsposed实现重读配置
     }
 }
